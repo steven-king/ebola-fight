438c4722ebab262a51b7084d166e029d5fee5296
steven-king <cujophoto@gmail.com>
2014-08-27 00:02:38 -0400

    making homepage work with JSON

---
+++ countries/liberia/site/js/main.js
@@ -0,0 +1,145 @@
+// load top level data for homepage
+var dataObj = new Array;
+var deaths = new Array;
+var cases = new Array;
+var contacts = new Array;
+var HCWDeaths = new Array;
+var HCWCases = new Array;
+var labs = new Array;
+var treatments = new Array;
+
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function loadData(){
+    $.ajax({
+      type:"GET",
+      url:"data/main.json",
+      dataType:"text",
+      success: parseData
+    });
+  }//end load(xml)
+
+      
+function parseData(data){
+    console.log("parseData")
+    dataObj = $.parseJSON(data);
+    
+       
+   
+    //death box top left
+    $("#total-deaths").text(numberWithCommas(dataObj[0]["Total Deaths"]));
+    var theDifference;
+    var thisWeek = dataObj[0]["Deaths This Week"];
+    var lastWeek = dataObj[0]["Deaths Last Week"];
+    if (thisWeek >= lastWeek) {
+        //increase
+        theDifference = thisWeek - lastWeek;
+        console.log("theDifference = " + theDifference);
+        $("#death-difference").html("&#9650; " + theDifference + " more deaths this week");
+    }
+    if ( thisWeek <= lastWeek) {
+        //decrease
+        theDifference = lastWeek - thisWeek;
+        $("#death-difference").html("&#9650;" + theDifference + " less deaths this week");
+    }
+    if ( thisWeek == lastWeek) {
+        //same
+        $("#death-difference").html("no change week to week");
+    }
+    
+    
+    //case box top left
+    $("#total-cases").text(numberWithCommas(dataObj[1]["Total Cases"]));
+    var theDifference;
+    var thisWeek = dataObj[1]["Cases This Week"];
+    var lastWeek = dataObj[1]["Cases Last Week"];
+    if (thisWeek >= lastWeek) {
+        //increase
+        theDifference = thisWeek - lastWeek;
+        console.log("theDifference = " + theDifference);
+        $("#case-difference").html("&#9650; " + theDifference + " more cases week");
+    }
+    if ( thisWeek <= lastWeek) {
+        //decrease
+        theDifference = lastWeek - thisWeek;
+        $("#case-difference").html("&#9650;" + theDifference + " less cases this week");
+    }
+    if ( thisWeek == lastWeek) {
+        //same
+        $("#case-difference").html("no change week to week");
+    }
+    
+    
+    
+    
+    
+    //labs box top left
+    //$("#total-deaths").text(dataObj[0]["Total Deaths"]);
+    
+    
+    
+    var theDifference;
+    
+    var tlp = dataObj[5]["Total Labs Processed"];
+    var lpThisWeek = dataObj[5]["Labs Processed This Week"];
+    var lpTlastWeek = dataObj[5]["Labs Processed Last Week"];
+    
+    var tlc = dataObj[5]["Total Labs Collected"];
+    var lcThisWeek = dataObj[5]["Labs Collected This Week"];
+    var lcLastWeek = dataObj[5]["Labs Collected Last Week"];
+    
+    console.log(tlp);
+    var labRate = (tlc - tlp) / tlc * 100 +"%" 
+    
+    $("#lab-rate").text(labRate);
+    $("#lab-details").html("Collected: " + tlc + "  <br/>  Tested: " + tlp);
+    
+    
+    
+
+    
+    var theDifference;
+    
+    var totalContacts = dataObj[2]["Total Contacts"];
+    var totalCases = dataObj[1]["Total Cases"];
+
+    
+    console.log(tlp);
+    var exposureRate = (totalContacts - totalCases ) / totalCases * 100  
+    var newRate = Math.round(exposureRate)+"%";
+    $("#exposure-rate").text(newRate);
+    $("#exposure-details").html("Cases: " + totalCases + "  <br/>  Contacts: " + totalContacts);
+    
+    
+    
+    
+    
+    //CFR box top right
+    $("#cfr-rate").text(dataObj[6]["FCR"] + "%");
+    var theDifference;
+    var thisWeek = dataObj[6]["FCR This Week"];
+    var lastWeek = dataObj[6]["FCR Last Week"];
+    if (thisWeek >= lastWeek) {
+        //increase
+        theDifference = thisWeek - lastWeek;
+        console.log("theDifference = " + theDifference);
+        $("#cfr-difference").html("&#9650; " + theDifference + " more cases this week");
+    }
+    if ( thisWeek <= lastWeek) {
+        //decrease
+        theDifference = lastWeek - thisWeek;
+        $("#cfr-difference").html("&#9650;" + theDifference + " less cases this week");
+    }
+    if ( thisWeek == lastWeek) {
+        //same
+        $("#cfr-difference").html("no change week to week");
+    }
+    
+
+}
+
+
+
