7c7cd5a8100bcb65b8b38e6ef6f37b1ba69f1007
steven-king <cujophoto@gmail.com>
2014-08-31 16:52:06 -0400

    data changes to charts

--- countries/liberia/site/js/main.js
+++ countries/liberia/site/js/main.js
@@ -38,8 +38,9 @@ function parseData(data){
  
     var theDifference = 231;
     var percentChange = 9;
-
-    $("#death-difference").html("Up " + theDifference + " this week <br/>(" + percentChange + "% weekly increase)");
+    $("#death-difference").html(theDifference + " new deaths <br/>in last 7 days <br/>(" + percentChange + "% increase this week)");
+    
+    //$("#death-difference").html("Up " + theDifference + " this week <br/>(" + percentChange + "% weekly increase)");
     
     /*
     var thisWeek = dataObj[0]["Deaths This Week"];
@@ -66,7 +67,8 @@ function parseData(data){
     var theDifference = 460;
     var percentChange = 55;
 
-    $("#case-difference").html("Up " + theDifference + " this week <br/>(" + percentChange + "% weekly increase)");
+    $("#case-difference").html(theDifference + " new cases <br/>in last 7 days <br/>(" + percentChange + "% increase this week)");
+    
     /*
     var thisWeek = dataObj[1]["Cases This Week"];
     
@@ -92,7 +94,7 @@ function parseData(data){
     var theDifference = 8;
     var percentChange = 25;
 
-    $("#hcw-death-details").html("Down " + theDifference + " this week <br/>(" + percentChange + "% decrease)");
+    $("#hcw-death-details").html(theDifference + " new deaths <br/>in last 7 days <br/>(" + percentChange + "% decrease this week)");
     
     /*
     var theDifference;
@@ -120,7 +122,7 @@ function parseData(data){
     var theDifference = 20;
     var percentChange = 27;
 
-    $("#hcw-cases-details").html("Up " + theDifference + " this week <br/>(" + percentChange + "% increase)");
+    $("#hcw-cases-details").html(theDifference + " new cases <br/>in last 7 days <br/>(" + percentChange + "% increase this week)");
     
     /*
     var theDifference;
@@ -147,78 +149,6 @@ function parseData(data){
     
     //labs box top left
     //$("#total-deaths").text(dataObj[0]["Total Deaths"]);
-    
-    
-    
-    var theDifference;
-    
-    var tlp = dataObj[5]["Total Labs Processed"];
-    var lpThisWeek = dataObj[5]["Labs Processed This Week"];
-    var lpTlastWeek = dataObj[5]["Labs Processed Last Week"];
-    
-    var tlc = dataObj[5]["Total Labs Collected"];
-    var lcThisWeek = dataObj[5]["Labs Collected This Week"];
-    var lcLastWeek = dataObj[5]["Labs Collected Last Week"];
-    
-    console.log(tlp);
-    var labRate = (tlc - tlp) / tlc * 100 +"%" 
-    
-    $("#lab-rate").text(labRate);
-    $("#lab-details").html("Collected: " + tlc + "  <br/>  Tested: " + tlp);
-    
-    
-    
-
-    
-    var theDifference;
-    
-    var totalContacts = dataObj[2]["Total Contacts"];
-    var totalCases = dataObj[1]["Total Cases"];
-
-    
-    console.log(tlp);
-    var exposureRate = (totalContacts - totalCases ) / totalCases * 100  
-    var newRate = Math.round(exposureRate)+"%";
-    $("#exposure-rate").text(newRate);
-    $("#exposure-details").html("Cases: " + totalCases + "  <br/>  Contacts: " + totalContacts);
-    
-    
-    
-    
-    
-    //CFR box lower  right
-    $("#cfr-rate").text(dataObj[6]["FCR"] + "%");
-    var theDifference;
-    var thisWeek = dataObj[6]["FCR This Week"];
-    var lastWeek = dataObj[6]["FCR Last Week"];
-    if (thisWeek >= lastWeek) {
-        //increase
-        theDifference = thisWeek - lastWeek;
-        console.log("theDifference = " + theDifference);
-        $("#cfr-difference").html("Up " + theDifference + " this week");
-    }
-    if ( thisWeek <= lastWeek) {
-        //decrease
-        theDifference = lastWeek - thisWeek;
-        $("#cfr-difference").html("Down" + theDifference + " this week");
-    }
-    if ( thisWeek == lastWeek) {
-        //same
-        $("#cfr-difference").html("no change <br/> week to week");
-    }
-    
-    
-     
-    //Clinics
-   
-    var theAverage;
-    var regions = 15;
-    var clinics  = dataObj[7]["Total Clinics"]
-    
-    theAverage = Math.round(clinics / regions); 
-    
-    $("#clinics").text(dataObj[7]["Total Clinics"]);
-    $("#clinics-details").html("Average of " + theAverage + "<br/> clinics per region");
     
     
     
